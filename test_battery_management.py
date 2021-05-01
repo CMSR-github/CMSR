5927074fec3e3d8a49b4f8953dc9119ad15261d0
Kobe <majorkobe@gmail.com>
2021-05-01 13:10:11 -0400

    'test'

---
+++ test_battery_management.py
@@ -0,0 +1,74 @@
+import time
+import board
+import busio
+#import numpy as np
+import json
+import random
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+#import matplotlib.pyplot as plt
+
+i2c = busio.I2C(board.SCL,board.SDA)
+ads = ADS.ADS1115(i2c)
+ads.mode = Mode.CONTINUOUS
+# this is directly affecting the reading values, replacing the opamp
+ads.gain = 50
+chan = AnalogIn(ads,ADS.P0)
+
+class BatteryManagement:
+
+    def __init__(self):
+        self.prev_time = time.time()
+        self.cur_time = time.time()
+        self.BSoC = 5*3600
+        self.prev_val = 0
+        self.cur_val = 0
+        
+        self.accessData()
+        self.idx = 0
+        self.data_len = len(self.data)
+        self.done = False
+    
+    def get_BSoC(self):
+        if self.done:
+            return None
+       
+        # Update Time stamps
+        self.prev_time = self.cur_time
+        self.cur_time = time.time()
+        dt = self.cur_time - self.prev_time
+        
+        # Update Current Measurements
+        self.cur_val = self.get_measurement()
+        dCharge = (self.prev_val+self.cur_val)/2 * dt
+        self.BSoC -= dCharge
+
+        return (self.BSoC/(5*3600))*100
+    
+    def accessData(self):
+        with open('RandomData.txt') as json_file:
+            self.data = json.load(json_file)
+    
+    def get_measurement_original(self):
+        data = self.data
+        val = data[self.idx]
+        self.idx += 1
+        if self.idx == self.data_len:
+            self.done = True
+        return val
+
+    def get_measurement(self):
+        val = chan.value
+        
+
+
+if __name__ == '__main__':
+    BSoC = 0
+    BMS = BatteryManagement()
+    while BSoC != None:
+        BSoC = BMS.get_BSoC()
+        time.sleep(0.1)
+        
+        print(f'Current Percent:{BSoC}')
+    
+    
