506fc8e537d98bb28a86197909283efcae1b0fcc
Kobe <majorkobe@gmail.com>
2022-03-27 14:15:06 -0400

    update to adc file

--- BatteryManagementADC.py
+++ BatteryManagementADC.py
@@ -1,8 +1,25 @@
 import time
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xx.analog_in import AnalogIn
 #import numpy as np
 import json
 import random
 #import matplotlib.pyplot as plt
+
+#create the SPI Bus
+spi = busio.SPI(clock = board.SCK, MISO = board.MISO, MOSI = board.MOSI)
+
+#create the chip select
+cs = digitalio.DigitalInOut(board.D22)
+
+#create mcp object
+mcp = MCP.MCP3008(spi,cs)
+#create analog input on pin 0
+chan0 = AnalogIn(mcp,MCP.P0)
+
 
 class BatteryManagement:
 
@@ -37,13 +54,29 @@ class BatteryManagement:
     def accessData(self):
         with open('RandomData.txt') as json_file:
             self.data = json.load(json_file)
+
+    #remap the value from the old range of values (raw values) to new range
+    def remap(self,value,orig_min,orig_max,new_min,new_max):
+        orig_range = orig_max - orig_min
+        new_range = new_max - new_min
+
+        #convert
+        valueScaled = int(value-orig_min) / int(orig_range)
+        new_val = int(new_min + (valueScaled*new_range))
+
+        return new_val
+
     
     def get_measurement(self):
-        data = self.data
-        val = data[self.idx]
-        self.idx += 1
-        if self.idx == self.data_len:
-            self.done = True
+
+        raw_val = chan0.value
+        val = raw_val
+        print(f"raw val: {raw_val}")
+        # data = self.data
+        # val = data[self.idx]
+        # self.idx += 1
+        # if self.idx == self.data_len:
+        #     self.done = True
         return val
         
 
